b270dcc728363d50d7d63acb6ae722bad90a7a5c
Karl Brightman <karl@karlbright.org>
2011-08-24 11:36:08 +0800

    Initial hello world

---
+++ 1_hello_world.js
@@ -0,0 +1,8 @@
+// # 1. Hello World
+
+// Write 'Hello World' to STDOUT.
+// Execute it with 'node 1_hello_world.js'
+
+console.log("Hello World"); // Hello World
+
+// Boring stuff, let's write some real stuff
